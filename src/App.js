fe912efb6569ddb7360172b849f3ce87792e1785
babuchirra12 <babuchirragithub@gmail.com>
2022-03-04 23:33:48 +0530

    sample react-toolkit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/home'
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi</h1>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
