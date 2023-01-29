713b30c5561f16fa627e7f2bc7e9e60ca2e1201a
babuchirra12 <babuchirragithub@gamil.com>
2023-01-29 20:14:24 +0530

    First Commit

--- src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-import logger from 'loglevel';
-
-const getLogger = (module) =>{
-    const log = logger.getLogger(module)
-    process.env.NODE_ENV === 'production' ?
-    log.setDefaultLevel(process.env.REACT_APP_LOG_LEVEL_PROD) :
-    log.setDefaultLevel(process.env.REACT_APP_LOG_LEVEL_DEV)
-}
